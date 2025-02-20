3d4e4bcd8a6ab847911de252ec432e15ad2745cb
Joseph Quiroz <joqu6657@stu.sjcc.edu>
2025-02-20 07:37:05 -0800

    Interface
    Created code for user interface

---
+++ src/demo/src/main/java/com/example/App.java
@@ -0,0 +1,26 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "------------------------------" );
+        System.out.println( "Chavvi Calc" );
+        System.out.println( "------------------------------" );
+        System.out.println( "A = 0.000      "+"B = 0.000" );
+        System.out.println( "------------------------------" );
+        System.out.println( "a     Enter a value for a" );
+        System.out.println( "b     Enter a value for b" );
+        System.out.println( "+     Add" );
+        System.out.println( "-     Subtract" );
+        System.out.println( "*     Multiply" );
+        System.out.println( "/     Divide" );
+        System.out.println( "c     Clear" );
+        System.out.println( "q     Quit" );
+        System.out.println( "------------------------------" );
+    }
+}
